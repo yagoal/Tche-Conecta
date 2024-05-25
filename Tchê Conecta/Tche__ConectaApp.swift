bc97ddfa8752ae3fcc50a78ffcad021652bd4a81
yagoal <br.yago@gmail.com>
2024-05-25 00:15:15 -0300

    Initial Commit

---
+++ Tchê Conecta/Tche__ConectaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tche__ConectaApp.swift
+//  Tchê Conecta
+//
+//  Created by Yago Pereira on 25/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Tche__ConectaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
